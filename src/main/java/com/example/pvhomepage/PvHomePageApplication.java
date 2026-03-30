b34b86af0facde951936dc4f28f1138475789e75
Felix Eriksson <felix.eriksson1996@hotmail.com>
2026-03-30 12:37:47 +0200

    initial commit

---
+++ src/main/java/com/example/pvhomepage/PvHomePageApplication.java
@@ -0,0 +1,13 @@
+package com.example.pvhomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvHomePageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PvHomePageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pvhomepage/PvHomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pvhomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvHomePageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
