b734295e03bd67229f35e93d9a24aa62da179a39
Felix Eriksson <felix.eriksson1996@hotmail.com>
2026-04-01 10:12:26 +0200

    security filter update

--- src/main/java/configuration/Security.java
+++ src/main/java/configuration/Security.java
@@ -1,18 +1,23 @@
 package configuration;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//
-//public class Security {
-//    public SecurityFilterChain secFilter(HttpSecurity http){
-//        return http.authorizeHttpRequests(auth -> auth
-//                .requestMatchers("/", "/home", "/bokning").permitAll()
-//                .anyRequest().permitAll()
-//        ).build();
-//
-//    }
-//
-//}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+public class Security {
+    public SecurityFilterChain secFilter(HttpSecurity http){
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/home", "/bokning").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        ).formLogin(form -> form
+                .loginPage("/login")
+                .permitAll()
+        ).logout(logout -> logout.permitAll());
+
+        return http.build();
+    }
+
+}
